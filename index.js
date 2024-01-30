1f23449f399967601cd3611bf2ab5137947602ae
Ashim Gupta <ashim.gupta@tredence.com>
2024-01-31 04:03:25 +0530

    adding port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import axios from 'axios';
 import https from 'https'
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.static('build'))
