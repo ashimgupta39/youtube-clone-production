5c01cd506d3459c4cb4aa1a651121c3105e8b0c3
Ashim Gupta <ashim.gupta@tredence.com>
2024-01-31 03:28:35 +0530

    prod code

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+import https from 'https'
+
+const PORT = 5000;
+const app = express();
+
+app.use(express.static('build'))
+const corsOptions = {
+    origin: ["http://localhost:3000","https://youtube-clone-nine-vert.vercel.app/","https://youtube-clone-37armu1o9-ashim-guptas-projects.vercel.app/","https://youtube-clone-git-main-ashim-guptas-projects.vercel.app/"],
+    method: ["GET","POST"]
+};
+app.use(cors(corsOptions));
+
+const requestEndpoint = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+app.get('/getSearchResults', async (req, res) => {
+    // console.log(requestEndpoint+req.query.q)
+    try {
+        const response = await axios.get(requestEndpoint+req.query.q, { 
+            httpsAgent: new https.Agent({ rejectUnauthorized: false }) })
+        console.log(response.data)
+        const x = response.data
+        res.send(x)
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
